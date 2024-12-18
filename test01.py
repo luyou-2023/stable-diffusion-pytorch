c67e64eb9fe88ee0929830672d70caf87f9609cd
luyou7 <88639430+luyou7@users.noreply.github.com>
2024-12-18 10:43:51 +0800

    保险

--- test01.py
+++ test01.py
@@ -3,3 +3,4 @@ from stable_diffusion_pytorch import pipeline
 prompts = ["a photograph of an astronaut riding a horse"]
 images = pipeline.generate(prompts)
 images[0].save('output.jpg')
+# tar -xf data.v20221029.tar
