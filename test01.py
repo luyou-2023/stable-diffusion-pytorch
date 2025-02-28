c54b7b8800477ab188fd02311358b2e8d09ebe55
luyou7 <88639430+luyou7@users.noreply.github.com>
2025-02-28 15:52:46 +0800

    data

--- test01.py
+++ test01.py
@@ -1,6 +1,11 @@
+from PIL import Image
+
 from stable_diffusion_pytorch import pipeline
 
-prompts = ["a photograph of an astronaut riding a horse"]
+prompts = ["Visualize a Valentine's Day promotion, infused with a charming palette of reds and pinks. Picture an affectionate setting teeming with heart-shaped decorations, premium gift boxes, and exquisite roses. Incorporate modern and elegant typography amidst a neat and festive layout. An enticing promotional offer is prominently displayed, perfect for catching the eye of those browsing website banners."]
 images = pipeline.generate(prompts)
 images[0].save('output.jpg')
+
+image = Image.open('output.jpg')
+image.show()
 # tar -xf data.v20221029.tar
